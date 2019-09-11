5298abde2c80d67fca6c95a4217049c517317a50
justcallmebeef <bethanymitch10@gmail.com>
2019-09-11 12:01:10 -0600

    strict comparison

--- chapter_1/sandbox.js
+++ chapter_1/sandbox.js
@@ -1,27 +1,13 @@
-// booleans & comparisons 
-console.log(true, false, 'true', 'false');
+ let age = 25 
 
-// methods can return booleans 
-let email = 'luigi@thenetninja.co.uk';
-let names = ['mario', 'luigi', 'toad']; 
+ // loose comparison (different types can still be equal)
 
-// let result = email.includes('@')
-// let result = names.includes('luigi')
-// console.log(result)
+ console.log(age == 25);
+ console.log(age == '25');
 
+ //strict comparison (different types cannot be equal)
 
-//comparison operators 
-let age = 25; 
-
-// console.log(age == 25);
-// console.log(age == 30);
-// console.log(age != 30);
-// console.log(age > 20);
-// console.log(age < 20);
-// console.log(age <= 25); 
-
-let name = 'shaun'; 
-
-console.log(name == 'shaun');
-console.log(name == 'Shaun');
-console.log(name > 'crystal');
+ console.log(age === 25);
+ console.log(age === '25');
+ console.log(age !== 25);
+ console.log(age !== '25');
