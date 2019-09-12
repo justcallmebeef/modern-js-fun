6a44712845eaefa73c8acb34a344871f59f1907b
Bethany Mitch <bethanymitch10@gmail.com>
2019-09-12 13:55:46 -0600

    fixed git issues

--- chapter_1/sandbox.js
+++ chapter_1/sandbox.js
@@ -1,15 +1,4 @@
-//if statements 
-// const age = 25;
-
-// if(age > 20) {
-//     console.log('you are over 20 years old');
-// }
-
-// const ninjas = ['shaun', 'ryu', 'chun-li', 'yoshi']; 
-
-// if(ninjas.length > 3) {
-//     console.log("that's a lot of ninjas")
-// }
+// else if statements 
 
 const password = 'password'; 
 
