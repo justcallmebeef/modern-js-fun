d7e03a28558bcf142892b78b84af15a647876243
Bethany Mitch <bethanymitch10@gmail.com>
2019-09-12 14:02:11 -0600

    else if basics

--- chapter_1/sandbox.js
+++ chapter_1/sandbox.js
@@ -2,6 +2,10 @@
 
 const password = 'password'; 
 
-if(password.length >= 8) {
-    console.log('that password is long enough')
+if(password.length >= 12) {
+    console.log('that password is mighty strong'); 
+} else if (password.length >= 8) {
+    console.log('that password is long enough');
+} else {
+    console.log('password is not long enough')
 }
