83cf6cf1024654458092953942b086c8be607a0b
Bethany Mitch <bethanymitch10@gmail.com>
2019-09-13 12:06:30 -0600

    switch statement basics

--- chapter_1/sandbox.js
+++ chapter_1/sandbox.js
@@ -1,18 +1,24 @@
-// break and continue 
+//switch statements 
+const grade = '50'; 
 
-const scores = [50, 25, 0, 30, 100, 20, 10]; 
-
-for (let i = 0; i < scores.length; i++) {
-
-    if(scores[i] === 0) {
-        continue;
-    }
-
-    console.log('your score: ', scores[i]);
-
-    if(scores[i] === 100) {
-        console.log('congrats, you got the top score!');
+switch(grade) {
+    case 50:
+        console.log('you got an A!');
         break;
-    }
-    
+    case 'B':
+            console.log('you got a B!');
+            break;
+    case 'C':
+        console.log('you got a C!');
+        break;
+    case 'D':
+        console.log('you got a D.');
+        break;
+    case 'E':
+        console.log('you got an E...?');
+        break;
+    default: 
+        console.log('not a valid grade');
 }
+
+//switch statements use STRICT equality 
