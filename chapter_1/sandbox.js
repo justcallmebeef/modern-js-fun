18f8e76a1318dda7fee2b4f1ed0dc587a505a703
Bethany Mitch <bethanymitch10@gmail.com>
2019-09-13 10:42:59 -0600

    OR and AND basics

--- chapter_1/sandbox.js
+++ chapter_1/sandbox.js
@@ -1,11 +1,11 @@
-// else if statements 
+// logical operators - OR || and AND && 
 
-const password = 'password'; 
+const password = 'p@ssw'; 
 
-if(password.length >= 12) {
+if(password.length >= 12 && password.includes('@')) {
     console.log('that password is mighty strong'); 
-} else if (password.length >= 8) {
-    console.log('that password is long enough');
+} else if (password.length >= 8 || password.includes('@') && password.length >+ 5) {
+    console.log('that password is strong enough');
 } else {
-    console.log('password is not long enough')
+    console.log('password is not strong enough')
 }
