7bfb83e8561b8975c15e101fb4f29b7d00877e68
Bethany Mitch <bethanymitch10@gmail.com>
2019-09-13 12:15:01 -0600

    variables and block scope

--- chapter_1/sandbox.js
+++ chapter_1/sandbox.js
@@ -1,24 +1,16 @@
-//switch statements 
-const grade = '50'; 
+// variables & block scope 
+const age = 30; 
 
-switch(grade) {
-    case 50:
-        console.log('you got an A!');
-        break;
-    case 'B':
-            console.log('you got a B!');
-            break;
-    case 'C':
-        console.log('you got a C!');
-        break;
-    case 'D':
-        console.log('you got a D.');
-        break;
-    case 'E':
-        console.log('you got an E...?');
-        break;
-    default: 
-        console.log('not a valid grade');
+if(true) {
+    const age = 40;
+    const name = 'shaun'; 
+    console.log('inside 1st code block: ', age, name);
+    
+    if(true) {
+        const age = 50;
+        console.log('inside 2nd code block: ', age);
+        var test = 'hello'; // doesn't recognize block scope 
+    }
 }
 
-//switch statements use STRICT equality 
+console.log('outside of code block: ', age, name, test);
