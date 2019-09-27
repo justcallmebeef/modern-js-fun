170833a4781283ba4687dd3b9741d89f1ec662ef
Bethany Mitch <bethanymitch10@gmail.com>
2019-09-27 09:13:28 -0600

    function declarations and expressions

--- chapter_1/sandbox.js
+++ chapter_1/sandbox.js
@@ -1,16 +1,18 @@
-// variables & block scope 
-const age = 30; 
 
-if(true) {
-    const age = 40;
-    const name = 'shaun'; 
-    console.log('inside 1st code block: ', age, name);
-    
-    if(true) {
-        const age = 50;
-        console.log('inside 2nd code block: ', age);
-        var test = 'hello'; // doesn't recognize block scope 
-    }
+// function declaration 
+function greet() {
+    console.log('hello there'); 
 }
 
-console.log('outside of code block: ', age, name, test);
+// function expression 
+const speak = function() {
+    console.log('good day!');
+};
+
+greet(); 
+greet(); 
+greet(); 
+
+speak(); 
+speak(); 
+speak(); 
